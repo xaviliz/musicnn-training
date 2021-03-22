7005e4a6dafcab5c211ba7b71a376193d1f2e30b
Bomme <13520622+Bomme@users.noreply.github.com>
2021-03-22 15:32:06 +0100

    fix typos
    and remove profanity :D

--- src/evaluate.py
+++ src/evaluate.py
@@ -31,8 +31,8 @@ def prediction(config, experiment_folder, id2audio_repr_path, id2gt, ids):
     batch_streamer = pescador.ZMQStreamer(batch_streamer)
 
     # tensorflow: define model and cost
-    fuckin_graph = tf.Graph()
-    with fuckin_graph.as_default():
+    tf_graph = tf.Graph()
+    with tf_graph.as_default():
         sess = tf.Session()
         [x, y_, is_train, y, normalized_y, cost, _] = train.tf_define_model_and_cost(config)
         sess.run(tf.global_variables_initializer())
@@ -42,7 +42,7 @@ def prediction(config, experiment_folder, id2audio_repr_path, id2gt, ids):
         pred_array, id_array = np.empty([0, NUM_CLASSES_DATASET]), np.empty(0)
         for batch in tqdm(batch_streamer):
             pred, _ = sess.run([normalized_y, cost], feed_dict={x: batch['X'], y_: batch['Y'], is_train: False})
-            # make shure our predictions have are a np
+            # make sure our predictions have are a np
             # array with the proper shape
             pred = np.array(pred).reshape(-1, NUM_CLASSES_DATASET)
             pred_array = np.vstack([pred_array, pred])
@@ -89,7 +89,7 @@ def store_results(config, results_file, predictions_file, models, ids, y_pred, m
 
 if __name__ == '__main__':
     # which experiment we want to evaluate?
-    # Use the -l functionality to ensamble models: python arg.py -l 1234 2345 3456 4567
+    # Use the -l functionality to ensemble models: python arg.py -l 1234 2345 3456 4567
     parser = argparse.ArgumentParser()
     parser.add_argument('-l', '--list', nargs='+', help='List of models to evaluate', required=True)
 
