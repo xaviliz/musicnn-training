1ebafa1419ab67b65e0d14ab996501ec2cf379cb
Bomme <13520622+Bomme@users.noreply.github.com>
2021-04-07 18:05:41 +0200

    add a simple test for AUC computation

---
+++ test/test_shared.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+import shared
+
+
+def test_auc():
+    predicted = [
+        [0.2, 0.3, 0.5],
+        [0.2, 0.41, 0.39],
+        [0.2, 0.1, 0.7],
+        [0.8, 0.1, 0.1],
+        [0.4, 0.3, 0.3],
+    ]
+    groundtruth = [
+        [1, 0, 0],
+        [0, 1, 0],
+        [0, 0, 1],
+        [1, 0, 0],
+        [0, 1, 0],
+    ]
+    roc_auc, pr_auc = shared.auc_with_aggergated_predictions(groundtruth, predicted)
+    np.testing.assert_allclose(roc_auc, 0.8611111)
+    np.testing.assert_allclose(pr_auc, 0.84444444)
