73de6878eea1c45c8b11aaa63f69f7b64c971dbb
palonso <pablo.alonso@upf.edu>
2022-02-28 11:10:32 +0100

    Implement fixed_batch_size mode

--- src/feature_embeddings.py
+++ src/feature_embeddings.py
@@ -52,8 +52,11 @@ class EmbeddingFromMelSpectrogram:
 
         # For now we don't know how to convert EffNet from Pytorch to TensorFlow with
         # dynamic (i.e., arbitrary) batch size, so we use a fixed one.
+        self.fixed_batch_size = False
+
         if self.model_type == "effnet_b0_3M":
             self.batch_size = 64
+            self.fixed_batch_size = True
 
     def compute(self, audio_file):
         mel_spectrogram = self.mel_extractor.compute(audio_file)
@@ -71,9 +74,20 @@ class EmbeddingFromMelSpectrogram:
         for i in range(nbatches):
             start = i * self.batch_size
             end = min(batch.shape[0], (i + 1) * self.batch_size)
-            pool.set(self.input_layer, batch[start:end])
+            batch_len = end - start
+            if (batch_len != self.batch_size) and self.fixed_batch_size:
+                input_batch = np.zeros((self.batch_size, 1, self.x_size, self.y_size), dtype="float32")
+                input_batch[: batch_len] = batch[start:end]
+            else:
+                input_batch = batch[start:end]
+            pool.set(self.input_layer, input_batch)
             out_pool = self.model(pool)
-            embeddings.append(out_pool[self.output_layer].squeeze())
+            output_batch = out_pool[self.output_layer].squeeze()
+
+            if (batch_len != self.batch_size) and self.fixed_batch_size:
+                output_batch = output_batch[:batch_len]
+
+            embeddings.append(output_batch)
 
         return np.vstack(embeddings)
 
