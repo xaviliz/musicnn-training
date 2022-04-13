be1f837c754fb88c19acc4fa2b3161078beba6e2
palonso <pablo.alonso@upf.edu>
2022-04-13 17:49:08 +0200

    Fix bug in freeze model
    placeholder x shold be created within the `model` scope

--- src/train.py
+++ src/train.py
@@ -22,29 +22,23 @@ def write_summary(value, tag, step, writer):
 
 
 def tf_define_model_and_cost(config):
-    return model_and_cost(
-        config,
-        tf.placeholder(tf.float32, [None, config['xInput'], config['yInput']]),
-        tf.placeholder(tf.bool)
-    )
+    x_shape = [None, config['xInput'], config['yInput']]
+    return model_and_cost(config, x_shape, tf.placeholder(tf.bool))
 
 
 def tf_define_model_and_cost_freeze(config):
     if config['xInput'] == 1:
-        shape = [None, config['yInput']]
+        x_shape = [None, config['yInput']]
     else:
-        shape = [None, config['xInput'], config['yInput']]
+        x_shape = [None, config['xInput'], config['yInput']]
 
-    return model_and_cost(
-        config,
-        tf.placeholder(tf.float32, shape),
-        False
-    )
+    return model_and_cost(config, x_shape, False)
 
 
-def model_and_cost(config, x, is_train):
+def model_and_cost(config, x_shape, is_train):
     # tensorflow: define the model
     with tf.name_scope('model'):
+        x = tf.placeholder(tf.float32, x_shape)
         y_ = tf.placeholder(tf.float32, [None, config['num_classes_dataset']])
 
         # choose between transfer learning or fully trainable models
